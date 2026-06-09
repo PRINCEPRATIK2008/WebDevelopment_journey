43fa410980461702f92b21c8d4da15b61f81186c
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-09 12:15:34 +0530

    new

---
+++ JAVA SCRIPT/practice1.js
@@ -0,0 +1,42 @@
+let student = {
+    Name : "Jassi",
+    Number: 9230392 , 
+    Marks : 540
+};
+
+
+console.log(student.Name)
+console.log(student.Number)
+console.log(student.Marks)
+
+// let a = 56;
+// console.log(a + 1);
+
+let name = "prince";
+let numbr = 3;
+
+let result = name + numbr;
+
+console.log(result);
+console.log(typeof name , typeof numbr , typeof result);
+
+
+let meaning = {
+   hazardous:"Dangerous",
+   benignandbenovolent: "kind and generous",
+   happygolucky: "casualattitude",
+   predicaments: "medicalproblems",
+   featherinthecap: "sone pe suhaga",
+}
+
+console.log(meaning.hazardous);
+console.log(meaning.benignandbenovolent);
+console.log(meaning.happygolucky);
+console.log(meaning.predicaments);
+console.log(meaning.featherinthecap);
+
+const student= {
+     name : "prince",
+     age: 18,
+     city:"chandigarh",
+};
