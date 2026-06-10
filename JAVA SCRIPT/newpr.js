a58bb7dbec51360a43e1fa5912490a3c12b5ff0b
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-10 14:51:28 +0530

    neew2

---
+++ JAVA SCRIPT/newpr.js
@@ -0,0 +1,57 @@
+// question 1
+
+let age54 = 15
+
+if (age54 >= 10 && age54 <= 20) {
+    console.log("The age of person is between 10 and 20");
+}
+
+else {
+    console.log("The age is not matching with the given requirement");
+}
+
+// question 4;
+
+let c = 9;
+
+if (c % 3 == 0 || c % 2 == 0) {
+    console.log("The number is either divisible by 2 or 3");
+}
+
+else {
+    console.log("The number is neither a divisible of 2 or 3");
+}
+
+// question 3
+
+let p = 12;
+
+if (p % 3 == 0 && p % 2 == 0) {
+    console.log("The number is multiple of 2 and 3");
+}
+else {
+    console.log("The number is not multiple of 2 and 3");
+}
+
+// question 5
+
+let age = 19;
+
+age >= 18 ? console.log("You can drive") : console.log("You can not drive");
+
+// question 2
+
+let day = 2;
+
+if (day == 1) {
+    console.log("Shoulders+cardio");
+}
+else if (day == 2) {
+    console.log("Chest+Back+Cardio");
+}
+else if (day == 3) {
+    console.log("Legs+Cardio");
+}
+else {
+    console.log("PURE cardio");
+}
