cc00bd61e682cff17c6b9d16afe4ddb533b09613
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-11 20:15:24 +0530

    working on loops

---
+++ JAVA SCRIPT/index1.js
@@ -0,0 +1,33 @@
+console.log("I am tutorial on LOOPS");
+
+let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)    
+
+
+// parts of this loop (intitalizing , conditon , incrememt or decrement)
+for (let i = 0; i < 100; i++) {
+   console.log(a + i);
+}
+
+// let obj = {
+//     name: "Prince",
+//     role: "software engg.",
+//     company: "Calcxx"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+
+// for (const c of "Prince") {
+//     console.log(c)
+// }
+let i=0;
+while (i<6) {
+    console.log(i)
+    i++;
+}
