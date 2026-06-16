720c4da97da656c273150733a90b2be78f672561
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-16 16:15:03 +0530

    Saas Project

---
+++ JAVA SCRIPT/Saasproject.js
@@ -0,0 +1,78 @@
+/* Creating a business name generator by combining list of adjextives and shop name and another world 
+
+List of adjectives:
+crazy
+amazing 
+fire
+
+Shop name:
+Engine 
+foods
+garments
+
+Another word :
+Bros
+Limited 
+Hub
+
+*/
+
+// Solution:
+
+let randm1 = Math.random()
+let randm2 = Math.random()
+let randm3 = Math.random()
+
+let adj = {
+    a: "crazy",
+    b: "amazing",
+    c: "FileReader"
+}
+
+let shopname = {
+    d: "Engine",
+    e: "Foods",
+    f: "Garments"
+}
+
+let anothername = {
+    g: "Bros",
+    h: "Limited",
+    i: "Hub"
+}
+
+let adjresult;
+if (randm1 < 0.33) {
+    adjresult = adj["a"];
+}
+else if (randm1 < 0.66) {
+    adjresult = adj["b"];
+}
+else {
+    adjresult = adj["c"];
+}
+
+let shopnameresult;
+if (randm2 < 0.33) {
+    shopnameresult = shopname["d"];
+}
+else if (randm2 < 0.66) {
+    shopnameresult = shopname["e"];
+}
+else {
+    shopnameresult = shopname["f"];
+}
+
+let anothernameresult;
+if (randm3 < 0.33) {
+    anothernameresult = anothername["g"];
+}
+else if (randm3 < 0.66) {
+    anothernameresult = anothername["h"];
+}
+else {
+    anothernameresult = anothername["i"];
+}
+
+alert(`Your business name should be: ${adjresult} ${shopnameresult} ${anothernameresult}`)
+alert(`Thankyou for choosing us`)
