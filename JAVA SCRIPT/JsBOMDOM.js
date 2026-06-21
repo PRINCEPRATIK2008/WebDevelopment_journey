a34f38c294034017bf988aaa3ab4d2d4dc908819
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-22 01:08:42 +0530

    DOM and BOM working

---
+++ JAVA SCRIPT/JsBOMDOM.js
@@ -0,0 +1,14 @@
+console.log("Hello world")
+
+// targeting elements within the body:
+
+document.body.firstElementChild
+document.body.firestElementChild.children
+document.body.firstElementChild.childNodes
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.firstElementChild.children[3].parentElement
+
+
+
+
