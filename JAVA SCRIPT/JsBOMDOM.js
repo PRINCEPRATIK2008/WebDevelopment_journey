dfcbfed577623880fe4a4e7a7fb2deb09e083850
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-23 01:28:38 +0530

    Working on tables and its command (DOM)

--- JAVA SCRIPT/JsBOMDOM.js
+++ JAVA SCRIPT/JsBOMDOM.js
@@ -9,6 +9,31 @@ document.body.firstElementChild.children[3].nextElementSibling
 document.body.firstElementChild.children[3].previousElementSibling
 document.body.firstElementChild.children[3].parentElement
 
+let table = document.querySelector("table")
 
+table.rows
+table.cell
+table.rows[1].rowIndex
+tabl.rows[1].cells[0]
+table.caption
+table.tFoot
+table.tHead
+table.tBodies
+// table bodies serial no like
+table.tbodies[0]
+// then the body rows and all 
+table.tbodies[0].rows
+// targeting cells
+table.rows[0].cells
+// row index in the thead/tbody part 
+table.rows[0].sectionRowIndex
+// cell index
+table.rows[1].cells[0].cellIndex
 
+ /*index part can be easily remembered :
 
+ rowIndex         → poore table me row number
+ sectionRowIndex  → apne thead/tbody me row number
+ cellIndex        → apni row me column number
+ 
+*/
