c10a837d072bb6472834e0e81f7ddc43c534087a
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-06-25 02:02:06 +0530

    More example on new method

--- JAVA SCRIPT/factorialrevision.js
+++ JAVA SCRIPT/factorialrevision.js
@@ -10,14 +10,29 @@
 
 // another better new method
 
-let a = 6
+// let a = 6
 
-function factorial(number) {
-    let arr = Array.from(Array(number+1).keys())
-    console.log(arr.slice(1,))
-    let c = arr.slice(1,).reduce((a, b) => {
-        return a * b
+// function factorial(number) {
+//     let arr = Array.from(Array(number + 1).keys())
+//     console.log(arr.slice(1,))
+//     let c = arr.slice(1,).reduce((a, b) => {
+//         return a * b
+//     })
+//     return c
+// }
+// factorial(a)
+
+// question for more understanding of this method :square calculation of n numbers where n is input from user...
+let p = 10
+
+function square(number) {
+    let Nwarr = Array.from(Array(number + 1).keys())
+    console.log(Nwarr.slice(1,))
+    let o = Nwarr.slice(1,).map((x) => {
+        return x ** 2
     })
-    return c
+    console.log(o)
 }
-factorial(a)
+square(p);
+
+
