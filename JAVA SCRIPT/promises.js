7b5391919cb54cb8d3f306a45429ddb4856c889c
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-07-16 03:25:10 +0530

    02_Promises

--- JAVA SCRIPT/promises.js
+++ JAVA SCRIPT/promises.js
@@ -32,9 +32,9 @@ let prom2 = new Promise((resolve, reject) => {
 // })
 
 let p3 = Promise.all([prom1, prom2])
-p3.then((a)=>{
+p3.then((a) => {
     console.log(a)
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
 
@@ -45,3 +45,90 @@ p3.then((a)=>{
 // p.resolve() : HMWRK
 // p.reject() : HMWRK
 // quix : try all these api on your custom promises
+
+// For more practice
+let prom3 = new Promise((resolve, reject) => {
+    console.log("Downloading.....")
+    setTimeout(() => {
+        resolve("Download Completed")
+    }, 2000);
+})
+
+// 2>
+console.log("Start");
+
+let p = new Promise((resolve, reject) => {
+    console.log("Inside Promise");
+    resolve("Done");
+});
+
+console.log("End");
+
+// output: internaly stored value on execution 
+
+// Start
+// Insider Promise
+// End
+
+// foR Done to be print we have to execute this function p.then()
+
+// 3>
+console.log("1");
+
+let p = new Promise((resolve) => {
+    console.log("2");
+    resolve("Prince");
+    console.log("3");
+});
+
+console.log("4");
+
+p.then((value) => {
+    console.log(value);
+});
+
+console.log("5");
+
+// output: 1, 2, 3, 4, 5, Prince
+
+// 4>
+function getData(dataID, getNextData) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data", dataID)
+            resolve("success")
+            if (getNextData) {
+                getNextData()
+            }
+        }, 5000);
+    });
+}
+
+// promise-chain
+function asyncFunc1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`some data 1`)
+            resolve("success")
+        }, 4000);
+    })
+}
+function asyncFunc2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`some data2`)
+            resolve("success")
+        }, 4000);
+    })
+}
+console.log("fetching data1....")
+let p1 = asyncFunc1()
+p1.then((a) => {
+    // console.log(a)
+    console.log("fetching data2....")
+    let p2 = asyncFunc2()
+    p2.then((res) => {
+        // console.log(res)
+    })
+})
+
