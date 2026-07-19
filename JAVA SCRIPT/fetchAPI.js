e9e044fa89a64324eec12b406b3921bf23f7d8e5
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-07-20 01:49:59 +0530

    Currency-eXchange Project_01

--- JAVA SCRIPT/fetchAPI.js
+++ JAVA SCRIPT/fetchAPI.js
@@ -2,20 +2,22 @@ const URL = "https://catfact.ninja/fact";
 const fct = document.querySelector("#fact");
 const tch = document.querySelector(".btn")
 
-// const getFacts = async () => {
-//     console.log("getting data ......")
-//     let response = await fetch(URL);
-//     console.log(response);// JSON FORMAT.. 
-//     let data = await response.json();  //.json hume second promise return krta toh krke hum await use kiya...
-//     fct.innerText = data.fact;
-// };
+const getFacts = async () => {
+    console.log("getting data ......")
+    let response = await fetch(URL);
+    console.log(response);// JSON FORMAT.. 
+    let data = await response.json();  //.json hume second promise return krta toh krke hum await use kiya...
+    fct.innerText = data.fact;
+};
 
 // with promise func
-fetch(URL).then((response) => {
-    return response.json()
-}).then((data) => {
-    console.log(data);
-    fct.innerText = data.fact;
+// function getFacts() {
+//     fetch(URL).then((response) => {
+//         return response.json()
+//     }).then((data) => {
+//         console.log(data);
+//         fct.innerText = data.fact;
+//     });
+// }
 
-})
 tch.addEventListener("click", getFacts);
