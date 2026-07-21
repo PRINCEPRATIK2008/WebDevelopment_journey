479900927a3635ae80a376c987c505ef83b69fb6
PRINCEPRATIK2008 <princepratik66@gmail.com>
2026-07-22 02:23:59 +0530

    Final-Currency_exchanger

---
+++ JAVA SCRIPT/Currency.js
@@ -0,0 +1,62 @@
+const BASE_URL =
+    "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg")
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = true;
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected = true;
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+
+    let amount = document.querySelector(".amount input");
+    let amtValue = Number(amount.value);
+
+    if (amtValue === "" || amtValue < 1) {
+        amtValue = 1;
+        amount.value = "1";
+    }
+    // Example: USD
+    let fromCurrency = fromCurr.value.toLowerCase();
+
+    // Example: INR
+    let toCurrency = toCurr.value.toLowerCase();
+
+    // Example:
+    // https://.../currencies/usd.json
+    const URL = `${BASE_URL}/${fromCurrency}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    // Get exchange rate from USD to INR
+    let exchangeRate = data[fromCurrency][toCurrency];
+    let finalAmount = amtValue * exchangeRate;
+    console.log(finalAmount);
+    msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+});
